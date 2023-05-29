627a7c648ff07689e7501b94d16ea91af4a09ecd
Islam Hesham <129389116+IslamHesham-Dev@users.noreply.github.com>
2023-05-29 11:53:38 -0700

    Add files via upload

---
+++ The Last Of Us Legacy/Milestone2-Solution/src/model/collectibles/Vaccine.java
@@ -0,0 +1,31 @@
+package model.collectibles;
+
+import java.awt.Point;
+
+import engine.Game;
+import model.characters.Character;
+import model.characters.Hero;
+import model.world.Cell;
+import model.world.CharacterCell;
+
+public class Vaccine implements Collectible {
+
+	@Override
+	public void pickUp(Hero h) {
+		h.getVaccineInventory().add(this);
+	}
+
+	@Override
+	public void use(Hero h) {
+		h.getVaccineInventory().remove(this);
+		Point p = h.getTarget().getLocation();
+		Cell cell = Game.map[p.x][p.y];
+		Game.zombies.remove(h.getTarget());
+		Hero tba = Game.availableHeroes.get((int) (Math.random() * Game.availableHeroes.size()));
+		Game.availableHeroes.remove(tba);
+		Game.heroes.add(tba);
+		((CharacterCell) cell).setCharacter(tba);
+		tba.setLocation(p);
+	}
+
+}
